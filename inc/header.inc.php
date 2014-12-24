709d36eefcdfc3f35e7bea4ae185630128bf450b
kazes <florian.boudot@gmail.com>
2014-12-24 20:07:17 +0100

    header (only started))

--- inc/header.inc.php
+++ inc/header.inc.php
@@ -1,5 +1,11 @@
 <div id="header">
+    <div class="page-width">
 
-   php included header<br>----<br><br>
-   
-</div><!-- #header -->
+        <h1 id="logo-berenicek">
+            <a href="#">
+                <img src="img/data/berenicek-logo.png" alt="Bérénice K. Directrice Artistique e-commerce">
+            </a>
+        </h1>
+    </div>
+</div>
+
