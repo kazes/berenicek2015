9f7cd50c9490053122c090b1bab9dd9e0999d988
kazes <florian.boudot@gmail.com>
2015-01-06 18:31:39 +0100

    header menu (not finished)

--- inc/header.inc.php
+++ inc/header.inc.php
@@ -1,11 +1,43 @@
+<!-- HEADER -->
 <div id="header">
     <div class="page-width">
 
+        <!-- LOGO -->
         <h1 id="logo-berenicek">
             <a href="#">
                 <img src="img/data/berenicek-logo.png" alt="Bérénice K. Directrice Artistique e-commerce">
             </a>
         </h1>
+
+
+        <!-- MENU -->
+        <div id="menu-container">
+            <ul class="menu left">
+                <li class="item">
+                    <a href="#">
+                        Porfolio
+                    </a>
+                </li>
+                <li class="item">
+                    <a href="#">
+                        CV & contact
+                    </a>
+                </li>
+            </ul>
+
+            <ul class="menu right">
+                <li class="item">
+                    <a href="#">
+                        Drawings
+                    </a>
+                </li>
+                <li class="item">
+                    <a href="#">
+                        Pinterest
+                    </a>
+                </li>
+            </ul>
+        </div>
     </div>
 </div>
 
