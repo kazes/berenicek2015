b52e58bb71ce2f7d536c83124890a7bc9589ba7b
kazes <florian.boudot@gmail.com>
2015-09-04 11:34:32 +0200

    delete script "scrolltofixed"

--- inc/scripts.inc.php
+++ inc/scripts.inc.php
@@ -1,6 +1,5 @@
 <!-- JQUERY javascript library -->
 <script src="js/jquery-2.1.0.js"></script>
-<script src="js/jquery-scrolltofixed-min.js"></script>
 
 <!-- all custom scripts in base.js -->
 <script src="js/base.js"></script>
