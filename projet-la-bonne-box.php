a3ab78aff6fd0d57a83dae0e263aba0cdc3e74c4
kazes <florian.boudot@gmail.com>
2015-09-15 16:51:42 +0200

    create file "functions.inc.php"

--- projet-la-bonne-box.php
+++ projet-la-bonne-box.php
@@ -19,22 +19,46 @@
                 <img src="img/data/projects/logo-la-bonne-box.png" alt="La Bonne Box" class="logo-project">
             </div>
 
-
-
             <!-- SECTION - STRATEGIE -->
-            <?php echo buildSsectionKeyword("stratégie", "Conseil stratégique & refonte de l’identité web d’une Box d’épicerie fine pour épicurieux.", array("img/data/projects/bonnebox/homepage-ordi.png")); ?>
+            <?php echo buildSsectionKeyword(
+                "stratégie",
+                "Conseil stratégique & refonte de l’identité web d’une Box d’épicerie fine pour épicurieux.",
+                array("img/data/projects/bonnebox/homepage-ordi.png")
+            ); ?>
 
             <!-- SECTION - CHARTE -->
-            <?php echo buildSsectionKeyword("charte", "Création d’une charte photographique pour les visuels d’ambiance ainsi qu’une charte pour les packshots des produits.", array("img/data/projects/bonnebox/charte-photo.png")); ?>
+            <?php echo buildSsectionKeyword(
+                "charte",
+                "Création d’une charte photographique pour les visuels d’ambiance ainsi qu’une charte pour les packshots des produits.",
+                array("img/data/projects/bonnebox/charte-photo.png")
+            ); ?>
 
             <!-- SECTION - HOME PAGE -->
-            <?php echo buildSsectionKeyword("homepage", "Nouvelle maquette web plus axée sur l’envie réalisée en collaboration avec Virginie Barnay Duhamel.", array("img/data/projects/bonnebox/homepage.jpg")); ?>
+            <?php echo buildSsectionKeyword(
+                "homepage",
+                "Nouvelle maquette web plus axée sur l’envie réalisée en collaboration avec Virginie Barnay Duhamel.",
+                array("img/data/projects/bonnebox/homepage.jpg")
+            ); ?>
 
             <!-- SECTION - E-COMMERCE -->
-            <?php echo buildSsectionKeyword("e-commerce", "Création d’outils marketing, newsletters, emails reflex, etc. et supervision de leur déclinaison par l’équipe graphique.", array("img/data/projects/bonnebox/abonnement.png", "img/data/projects/bonnebox/page-produit.jpg")); ?>
+            <?php echo buildSsectionKeyword(
+                "e-commerce",
+                "Création d’outils marketing, newsletters, emails reflex, etc. et supervision de leur déclinaison par l’équipe graphique.",
+                array(
+                    "img/data/projects/bonnebox/abonnement.png",
+                    "img/data/projects/bonnebox/page-produit.jpg"
+                )
+            ); ?>
 
             <!-- SECTION - SHOOTINGS -->
-            <?php echo buildSsectionKeyword("shootings", "Direction artistique de shootings d’après moodboards. Constitution d’une équipe avec une photographe et une styliste photo.", array("img/data/projects/bonnebox/abonnement.png", "img/data/projects/bonnebox/shootings.jpg")); ?>
+            <?php echo buildSsectionKeyword(
+                "shootings",
+                "Direction artistique de shootings d’après moodboards. Constitution d’une équipe avec une photographe et une styliste photo.",
+                array(
+                    "img/data/projects/bonnebox/abonnement.png",
+                    "img/data/projects/bonnebox/shootings.jpg"
+                )
+            ); ?>
 
             <!-- FOOTER - CONTACT -->
             <p class="bt-contact">
