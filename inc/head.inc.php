59adf52fc41ab77818b532ed38abdfbf8e8e3ad7
kazes <florian.boudot@gmail.com>
2015-09-15 16:45:29 +0200

    page BONNE BOX : with function buildSectionKeyword to write posts

--- inc/head.inc.php
+++ inc/head.inc.php
@@ -14,3 +14,42 @@
         htmlTag.className = (htmlTag.className + ' ' || '') + 'hasJS';
     }();
 </script>
+
+
+<?php
+
+/**
+ * @param string $keyword
+ * @param string $title
+ * @param array $img_src
+ * @return string
+ */
+function buildSsectionKeyword($keyword = 'keyword', $title = 'title', $img_src = array('')){
+    // get images url
+    $images = '';
+    for ($i = 0; $i < count($img_src); $i++) {
+        $images .= '<img src="'.$img_src[$i].'" alt="'.$title.'">';
+    }
+
+    // build html with these vars
+    $html = <<<"CONTENT"
+
+    <div class="section">
+        <div class="inner">
+            <h2 class="keyword title-4">
+                {$keyword}
+            </h2>
+
+            <h3 class="description title-2">
+                {$title}
+            </h3>
+
+            {$images}
+        </div>
+    </div>
+CONTENT;
+
+    // boom
+    return $html;
+}
+?>
--- projet-la-bonne-box.php
+++ projet-la-bonne-box.php
@@ -10,6 +10,7 @@
         <?php include('inc/header.inc.php'); ?>
         
         <div id="page" class="page-width page-project">
+            <!-- INTRO -->
             <div class="intro">
                 <h1 class="title-3">
                     Refonte de l’identité web de la Bonne Box
@@ -18,21 +19,24 @@
                 <img src="img/data/projects/logo-la-bonne-box.png" alt="La Bonne Box" class="logo-project">
             </div>
 
-            <div class="section">
-                <h2 class="title-4">
-                    stratégie
-                </h2>
-
-                <h3 class="title-2">
-                    Conseil stratégique & refonte de l’identité web d’une Box d’épicerie fine pour épicurieux.
-                </h3>
-
-                <img src="img/data/projects/bonne-box-screenshot-0.png" alt="La Bonne Box">
-            </div>
 
 
+            <!-- SECTION - STRATEGIE -->
+            <?php echo buildSsectionKeyword("stratégie", "Conseil stratégique & refonte de l’identité web d’une Box d’épicerie fine pour épicurieux.", array("img/data/projects/bonnebox/homepage-ordi.png")); ?>
 
-            <!-- CONTACT -->
+            <!-- SECTION - CHARTE -->
+            <?php echo buildSsectionKeyword("charte", "Création d’une charte photographique pour les visuels d’ambiance ainsi qu’une charte pour les packshots des produits.", array("img/data/projects/bonnebox/charte-photo.png")); ?>
+
+            <!-- SECTION - HOME PAGE -->
+            <?php echo buildSsectionKeyword("homepage", "Nouvelle maquette web plus axée sur l’envie réalisée en collaboration avec Virginie Barnay Duhamel.", array("img/data/projects/bonnebox/homepage.jpg")); ?>
+
+            <!-- SECTION - E-COMMERCE -->
+            <?php echo buildSsectionKeyword("e-commerce", "Création d’outils marketing, newsletters, emails reflex, etc. et supervision de leur déclinaison par l’équipe graphique.", array("img/data/projects/bonnebox/abonnement.png", "img/data/projects/bonnebox/page-produit.jpg")); ?>
+
+            <!-- SECTION - SHOOTINGS -->
+            <?php echo buildSsectionKeyword("shootings", "Direction artistique de shootings d’après moodboards. Constitution d’une équipe avec une photographe et une styliste photo.", array("img/data/projects/bonnebox/abonnement.png", "img/data/projects/bonnebox/shootings.jpg")); ?>
+
+            <!-- FOOTER - CONTACT -->
             <p class="bt-contact">
                 <a href="#">
                     Racontez-moi votre projet <span class="excla">!</span>
