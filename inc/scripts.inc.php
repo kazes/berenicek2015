d710d81f1f4fe4ccabd89c76f472a17f684d8336
kazes <florian.boudot@gmail.com>
2015-10-05 17:26:03 +0200

    optim : replace jquery with minified file

--- inc/scripts.inc.php
+++ inc/scripts.inc.php
@@ -1,5 +1,5 @@
 <!-- JQUERY javascript library -->
-<script src="js/jquery-2.1.0.js"></script>
+<script src="js/jquery-2.1.4.min.js"></script>
 
 <!-- all custom scripts in base.js -->
 <script src="js/base.js"></script>
