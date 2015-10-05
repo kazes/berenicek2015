87a7aea158e311750c303ef9fb4b7e0f9d4128e1
kazes <florian.boudot@gmail.com>
2015-10-05 11:30:55 +0200

    footer : format code #16

--- inc/footer.inc.php
+++ inc/footer.inc.php
@@ -1,5 +1,6 @@
-
 </div><!-- #ajax -->
+
+<!-- FOOTER -->
 <div id="footer">
     <div class="inside page-width">
         <div class="column column-1">
@@ -7,46 +8,64 @@
                 <h2 class="title">
                     <a href="index" class="history">Portfolio</a>
                 </h2>
-                <ul class="list"><li class="item item-0">
+                <ul class="list">
+                    <li class="item item-0">
                         <a href="projet-usine-a-design" class="link history">L’Usine à Design</a>
-                    </li><li class="item item-1">
+                    </li>
+                    <li class="item item-1">
                         <a href="projet-la-bonne-box" class="link history">La Bonne Box</a>
-                    </li><li class="item item-2">
+                    </li>
+                    <li class="item item-2">
                         <a href="projet-new-kidz" class="link history">New Kidz Stories</a>
-                    </li><li class="item item-3">
+                    </li>
+                    <li class="item item-3">
                         <a href="projet-micromix" class="link history">Micromix</a>
-                    </li><li class="item item-4">
+                    </li>
+                    <li class="item item-4">
                         <a href="projet-brandalley" class="link history">Brandalley</a>
-                    </li></ul>
-            </div>        </div>
+                    </li>
+                </ul>
+            </div>
+        </div>
         <div class="column column-2">
             <div class="section" id="ft-section-2">
                 <h2 class="title">
-                    <a href="contact" class="history"> CV & Contact</a>
+                    <a href="contact" class="history">CV & Contact</a>
                 </h2>
-                <ul class="list"><li class="item item-0">
-                        <a href="pdf/berenice-krzyzaniak-boudot-cv.pdf" class="link history">Télécharger mon CV en pdf</a>
-                    </li><li class="item item-1">
+                <ul class="list">
+                    <li class="item item-0">
+                        <a href="pdf/berenice-krzyzaniak-boudot-cv.pdf" class="link history">Télécharger mon CV en
+                            pdf</a>
+                    </li>
+                    <li class="item item-1">
                         <a href="contact" class="link history">Contactez-moi</a>
-                    </li></ul>
-            </div><div class="section" id="ft-section-3">
+                    </li>
+                </ul>
+            </div>
+            <div class="section" id="ft-section-3">
                 <h2 class="title">
 
                 </h2>
                 <ul class="list"></ul>
-            </div>        </div>
+            </div>
+        </div>
         <div class="column column-3">
             <div class="section" id="ft-section-4">
                 <h2 class="title">
                     Suivez-moi
                 </h2>
-                <ul class="list"><li class="item item-0">
+                <ul class="list">
+                    <li class="item item-0">
                         <a href="http://www.pinterest.com/berenicek" class="link history">pinterest</a>
-                    </li><li class="item item-1">
+                    </li>
+                    <li class="item item-1">
                         <a href="http://instagram.com/sweetberenicek/" class="link history">instagram</a>
-                    </li></ul>
-            </div>        </div>
+                    </li>
+                </ul>
+            </div>
+        </div>
 
+        <!-- CREDITS -->
         <p id="credits">
             Copyright bérénice krzyzaniak -
             design <a href="http://www.berenicek.com" target="_blank">berenicek.com</a> -
