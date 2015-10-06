f783eb28c2ced2090ee74beb6b481aeae8df52a7
kazes <florian.boudot@gmail.com>
2015-10-06 11:38:08 +0200

    header : menu buttons triangle cut + animation hover #14

--- inc/header.inc.php
+++ inc/header.inc.php
@@ -26,12 +26,12 @@
             <ul class="menu left line">
                 <li id="menu-portfolio" class="item <?php if($is_portfolio || $is_home){ ?>active<?php } ?>">
                     <a href="/" class="history">
-                        Portfolio
+                        <span class="text">Portfolio</span>
                     </a>
                 </li>
                 <li id="menu-contact" class="item <?php if($is_contact){ ?>active<?php } ?>">
                     <a href="contact" class="history">
-                        CV & contact
+                        <span class="text">CV & contact</span>
                     </a>
                 </li>
             </ul>
@@ -39,12 +39,12 @@
             <ul class="menu right line">
                 <li id="menu-drawings" class="item <?php if($is_drawings){ ?>active<?php } ?>">
                     <a href="drawings" class="history">
-                        Drawings
+                        <span class="text">Drawings</span>
                     </a>
                 </li>
                 <li class="item">
                     <a href="http://www.pinterest.com/berenicek/" target="_blank">
-                        Pinterest
+                        <span class="text">Pinterest</span>
                     </a>
                 </li>
             </ul>
